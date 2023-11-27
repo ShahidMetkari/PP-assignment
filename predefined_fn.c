ca7e5baf4ee68c4b4440db43064a9053016a5369
ShahidMetkari <148204776+ShahidMetkari@users.noreply.github.com>
2023-11-28 01:05:18 +0530

    Create predefined_fn.c

---
+++ predefined_fn.c
@@ -0,0 +1,15 @@
+•	#include <stdio.h>
+•	#include <conio.h>
+•	#include <math.h>
+•	
+•	int main() {
+•	    int x, y;
+•	    clrscr();
+•	    printf("Enter a positive number: ");
+•	    scanf("%d", &x);
+•	    y = sqrt(x);
+•	    printf("Square root = %d", y);
+•	    getch();
+•	    return 0;
+•	}
+
