6a9acb175704132e9e560b51458147e78ec8a4c1
ShahidMetkari <148204776+ShahidMetkari@users.noreply.github.com>
2023-11-28 01:07:23 +0530

    Create userdefined_fn.c

---
+++ userdefined_fn.c
@@ -0,0 +1,16 @@
+2.	#include <iostream>
+3.	using namespace std;
+4.	
+5.	// Function declaration (prototype)
+6.	void greetUser();
+7.	
+8.	int main() {
+9.	    // Call the function
+10.	    greetUser();
+11.	    return 0;
+12.	}
+13.	
+14.	// Function definition (body)
+15.	void greetUser() {
+16.	    cout << "Hello! Welcome to the world of functions." << endl;
+17.	}
