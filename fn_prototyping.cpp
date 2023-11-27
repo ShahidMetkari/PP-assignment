2fe614cfb11bf67205bd23aa956de8581a6d03c7
ShahidMetkari <148204776+ShahidMetkari@users.noreply.github.com>
2023-11-28 01:08:04 +0530

    Create fn_prototyping.cpp

---
+++ fn_prototyping.cpp
@@ -0,0 +1,14 @@
+•	// Function prototype
+•	int add(int a, int b);
+•	
+•	int main() {
+•	    // Function call
+•	    int result = add(5, 3);
+•	    // ...
+•	}
+•	
+•	// Function definition
+•	int add(int a, int b) {
+•	    return a + b;
+•	}
+
